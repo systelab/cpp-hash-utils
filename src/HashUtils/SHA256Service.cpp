03c93aa7f47632df1bd879476a39214ff8edffaf
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-07-13 11:18:42 +0200

    Added service implementation using OpenSSL

--- src/HashUtils/SHA256Service.cpp
+++ src/HashUtils/SHA256Service.cpp
@@ -1,5 +1,10 @@
 #include "SHA256Service.h"
 
+#include <iomanip>
+#include <sstream>
+
+#include <openssl/sha.h>
+
 
 namespace systelab { namespace hash_utils {
 
@@ -8,9 +13,19 @@ namespace systelab { namespace hash_utils {
 
 	std::string SHA256Service::computeHash(const std::string& value) const
 	{
-		value;
-		// TODO
-		return "";
+		unsigned char hash[SHA256_DIGEST_LENGTH];
+		SHA256_CTX sha256;
+		SHA256_Init(&sha256);
+		SHA256_Update(&sha256, value.c_str(), value.size());
+		SHA256_Final(hash, &sha256);
+
+		std::stringstream ss;
+		for (int i = 0; i < SHA256_DIGEST_LENGTH; i++)
+		{
+			ss << std::hex << std::setw(2) << std::setfill('0') << (int) hash[i];
+		}
+
+		return ss.str();
 	}
 
 }}
