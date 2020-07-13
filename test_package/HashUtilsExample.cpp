848f42fce53813f8f00b8615aa544dd69a1c8787
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-07-13 11:48:07 +0200

    Updated test package example to be based on factory.

--- test_package/HashUtilsExample.cpp
+++ test_package/HashUtilsExample.cpp
@@ -1,4 +1,5 @@
-#include "HashUtils/SHA256Service.h"
+#include "HashUtils/HashUtilsFactory.h"
+#include "HashUtils/IHashService.h"
 
 #include <iostream>
 #include <memory>
@@ -6,7 +7,7 @@
 
 int main(int argc, char *argv[])
 {
-    std::unique_ptr<systelab::hash_utils::IHashService> hashService = std::make_unique<systelab::hash_utils::SHA256Service>();
+	auto hashService = systelab::hash::HashUtilsFactory().buildSHA256HashService();
 
 	std::string input = "This is a super-secret to be hashed";
 	std::string output = hashService->computeHash(input);
