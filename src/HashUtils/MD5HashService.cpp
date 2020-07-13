7ba4daf273fde1108f1d3bba79044006bb3321ee
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-07-13 11:34:31 +0200

    Implemented service to compute the MD5.

--- src/HashUtils/MD5HashService.cpp
+++ src/HashUtils/MD5HashService.cpp
@@ -3,6 +3,7 @@
 #include <iomanip>
 #include <sstream>
 
+#include <openssl/md5.h>
 
 
 namespace systelab { namespace hash_utils {
@@ -12,9 +13,16 @@ namespace systelab { namespace hash_utils {
 
 	std::string MD5HashService::computeHash(const std::string& value) const
 	{
-		value;
-		// TODO
-		return "";
+		unsigned char hash[MD5_DIGEST_LENGTH];
+		MD5((unsigned char*) value.c_str(), value.size(), hash);
+
+		std::stringstream ss;
+		for (int i = 0; i < MD5_DIGEST_LENGTH; i++)
+		{
+			ss << std::hex << std::setw(2) << std::setfill('0') << (int) hash[i];
+		}
+
+		return ss.str();
 	}
 
 }}
