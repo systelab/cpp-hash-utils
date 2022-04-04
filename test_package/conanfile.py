98dff0c86175ecd9a0a3331c36dd646aa79b9eb4
jcarreras <jcarreras@werfen.com>
2022-04-04 17:52:48 +0200

    Fix test_package Conan recipe
    Fix test_package Conan recipe

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -16,7 +16,8 @@ class HashUtilsTestConan(ConanFile):
         cmake.build()
 
     def imports(self):
-        self.copy("*.dll", dst="bin", src="bin")
+        self.copy("*.dll", dst=("bin/%s" % self.settings.build_type), src="bin")
+        self.copy("*.dll", dst=("bin/%s" % self.settings.build_type), src="lib")
         self.copy("*.dylib*", dst="bin", src="lib")
         self.copy('*.so*', dst='bin', src='lib')
 
